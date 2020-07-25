67e39ea9daf86892277074e57eeb8cc070a19cb2
roshan <dtubt2k17@gmail.com>
2020-07-25 20:58:48 +0530

    upto mongo

---
+++ app_server/models/db.js
@@ -0,0 +1,61 @@
+//mongo "mongodb+srv://cluster0.7ewfw.mongodb.net/<dbname>" --username mongouser
+//mongodb+srv://mongouser:<password>@cluster0.7ewfw.mongodb.net/<dbname>?retryWrites=true&w=majority
+//mongodb+srv://mongouser:<password>@cluster0.7ewfw.mongodb.net/test
+const mongoose = require('mongoose');
+const host = process.env.DB_HOST || '127.0.0.1'
+const dbURL = `mongodb://${host}/Loc8r`;
+const readLine = require('readline');
+
+const connect = () => {
+  setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true }), 1000);
+}
+
+mongoose.connection.on('connected', () => {
+  console.log('connected');
+});
+
+mongoose.connection.on('error', err => {
+  console.log('error: ' + err);
+  return connect();
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('disconnected');
+});
+
+if (process.platform === 'win32') {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on ('SIGINT', () => {
+    process.emit("SIGINT");
+  });
+}
+
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close( () => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
+};
+
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
+});
+process.on('SIGTERM', () => {
+  gracefulShutdown('Heroku app shutdown', () => {
+    process.exit(0);
+  });
+});
+
+connect();
+
+require('./locations');
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const ctrlMain = require('../controllers/main');
-/* GET homepage. */
-router.get('/', ctrlMain.index);
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
+
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
+
 module.exports = router;
